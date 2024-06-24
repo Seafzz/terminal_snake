0f8b9b72b2a5da3b4ec276071c11d85b40d25590
Seafzz <163156309+Seafzz@users.noreply.github.com>
2024-06-24 20:09:02 +0000

    added a function that asked for the players name, and there after if they have played the game before, if yes skip to game if no, give instructions and ask if they are ready

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 import curses
 import random
 
+#Ask for the players name
+player_name = input("Please enter your name:")
 
+#Check if the player has played before:
+while True:
+    played_before = input("Have you played Snake before? (yes/no)").lower()
+    if played_before in {'yes', 'y' 'no', 'n'}:
+        break
+    else:
+        print("Invalid input. Please enter 'yes' or 'no")
+
+#Instructions for the player if they awnsered no
+if played_before in ['no', 'n']:
+    print("Welcome to Snake, " + player_name + "! Use the arrow keys to control the snake. You can hold down the arrows to make the snake go faster. Do not let the snake touch the sides of the game area or eat itself! The snake will grow longer each time it eats food. The game will end when the snake runs into the sides of the game area or eats itself. Good luck!")
+    input("Press enter to start the game")
