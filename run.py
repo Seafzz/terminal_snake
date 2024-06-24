4d057bddcb90f1e99bd2a8196fa8b7557fd3dfb8
Seafzz <163156309+Seafzz@users.noreply.github.com>
2024-06-24 20:14:14 +0000

    deffined the main function

--- run.py
+++ run.py
@@ -10,9 +10,16 @@ while True:
     if played_before in {'yes', 'y' 'no', 'n'}:
         break
     else:
-        print("Invalid input. Please enter 'yes' or 'no")
+        print("Invalid input. Please enter 'yes' or 'no'")
 
 #Instructions for the player if they awnsered no
 if played_before in ['no', 'n']:
     print("Welcome to Snake, " + player_name + "! Use the arrow keys to control the snake. You can hold down the arrows to make the snake go faster. Do not let the snake touch the sides of the game area or eat itself! The snake will grow longer each time it eats food. The game will end when the snake runs into the sides of the game area or eats itself. Good luck!")
-    input("Press enter to start the game")
+    input("If you are ready press enter to start the game")
+
+#Deffine Main function
+def main(stdscr):
+    #Make cursor invisible
+    curses.curs_set(0) 
+    stdscr.nodelay(1) #Make stscr.get non blocking
+    stdscr.timeout(100) #Refresh screen every 100 ms
